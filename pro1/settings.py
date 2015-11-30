9befc680a956488f64507f956aeb0ef44f952f53
Your Name <you@example.com>
2015-12-01 01:03:12 +0800

    first

--- pro1/settings.py
+++ pro1/settings.py
@@ -101,7 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'static').replace('\\','/')
 STATIC_ROOT = "127.0.0.1:8000/static/"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
