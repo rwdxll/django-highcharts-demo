a1ea31c267b3586c6bb93267f552fd8f9244419d
cuihuanhuan <1696539307@qq.com>
2015-12-04 12:33:30 +0800

    static django highcharts demo

--- pro1/views.py
+++ pro1/views.py
@@ -1,10 +1,7 @@
 #-*-coding:utf-8-*-
 
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 def index(request):
-	d={'1.htldxhzj.duapp.com': 9398,'gtxapi.cdn.duapp.com': 79496,'www.xxx.com': 2477070,'www.baidu.com': 1465,'www.bing.com': 777,'www.aaa.com': 1113101,'www.ccc.net.cn': 922,'www.zhanimei.ga': 29847,'www.zhanimei.ml': 40155,'www.zhasini.ml': 373436}
-	categories = d.keys()
-	data = d.values()
-	return render_to_response('index.html',{'categories':categories,'data':data})
+	return render(request,'index.html')
